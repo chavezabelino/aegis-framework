fcb4119818e73ab43907b5ec21affe47c3538655
Abelino Chavez <abelino.chavez@gmail.com>
2025-08-10 21:00:27 -0500

    fix(governance): remove seeded violation for green receipt

--- unauthorized-file.ts
+++
@@ -1,7 +0,0 @@
-// This file is deliberately placed in the wrong location to test governance enforcement
-// It should fail the path check and be moved to tools/ or cli/
-
-export function unauthorizedFunction() {
-  console.log("This function should not exist in the root directory");
-  return "violation";
-}
